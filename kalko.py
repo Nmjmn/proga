3b41f199f0613a0a0997353757247cda7fc969e0
Nmjmn <nmjmneue@gmail.com>
2025-05-18 17:04:25 +0300

    Update kalko.py

--- kalko.py
+++ kalko.py
@@ -1,24 +1,23 @@
-
 num1 = float(input("Sisesta esimene number: "))  
-mata = input("Sisesta (+, -, *, /): ")      
+mata1 = input("Sisesta (+, -, *, /): ")      
 num2 = float(input("Sisesta teine number: "))    
 
 if mata1 == "+":
     mata2 = num1 + num2
-    print("Tulemus on:", mata)
+    print("Tulemus on:", mata2)
 
-elif mata == "-":
+elif mata1 == "-":
     mata2 = num1 - num2
-    print("Tulemus on:", mata)
+    print("Tulemus on:", mata2)
 
 elif mata1== "*":
     mata2 = num1 * num2
-    print("Tulemus on:", mata)
+    print("Tulemus on:", mata2)
 
 elif mata1 == "/":
     if num2 != 0:
         mata2 = num1 / num2
-        print("Tulemus on:", mata)
+        print("Tulemus on:", mata2)
     else:
         print("Pole võimalik bro!")  
 
